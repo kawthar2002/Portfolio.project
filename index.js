a83c1cc0ac31bc8faada2adae24c510e626e70b4
kawthar2002 <k31082002@gmail.com>
2023-02-15 17:08:31 +0300

    projects script

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ window.addEventListener('scroll', () => {
     const header = document.querySelector('.header__wrapper');
     let offSetY = window.scrollY;
     let headerPosition = `translateY(${offSetY * 0.4}px)`;
-    console.log(offSetY);
     header.style.transform = headerPosition;
     if (offSetY >= 650) {
       header.style.opacity = 0;
@@ -17,3 +16,17 @@ window.addEventListener('scroll', () => {
   }
 });
 
+const projects = document.querySelectorAll('.project');
+projects.forEach((project) => {
+  project.addEventListener('click', () => {
+    window.addEventListener('click', function (e) {
+      if (!e.composedPath().includes(project)) {
+        projects.forEach((project) => {
+          project.classList.remove('scale');
+        });
+      } else if (project.classList.contains('scale')) {
+        project.classList.remove('scale');
+      } else project.classList.add('scale');
+    });
+  });
+});
