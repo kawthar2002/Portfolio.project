2a0a93f32316239a34f7ba71de7559ff7bc298c1
kawthar2002 <k31082002@gmail.com>
2023-02-15 15:53:32 +0300

    script

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 const buttonDarkMode = document.querySelector('.darkmode-button');
-
 buttonDarkMode.onclick = () => {
   buttonDarkMode.classList.toggle('darkmode-button--active');
   document.body.classList.toggle('dark');
 };
 
 window.addEventListener('scroll', () => {
-  const header = document.querySelector('.header__wrapper');
-  let offSetY = window.scrollY;
-  let headerPosition = `translateY(${offSetY * 0.4}px)`;
-  console.log(offSetY);
-  header.style.transform = headerPosition;
-  if (offSetY >= 650) {
-    header.style.opacity = 0;
-  } else header.style.opacity = 1;
+  if (window.outerHeight > 768) {
+    const header = document.querySelector('.header__wrapper');
+    let offSetY = window.scrollY;
+    let headerPosition = `translateY(${offSetY * 0.4}px)`;
+    console.log(offSetY);
+    header.style.transform = headerPosition;
+    if (offSetY >= 650) {
+      header.style.opacity = 0;
+    } else header.style.opacity = 1;
+  }
 });
+
